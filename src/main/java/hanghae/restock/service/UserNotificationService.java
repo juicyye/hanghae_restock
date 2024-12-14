45b7d54ef75ce2a45f5763587a5d6f408ec2fc7d
param <spsummerdoc@gmail.com>
2024-12-14 14:53:31 +0900

    refactor(userNotificationService): 마지막 재입고 유저 조회 로직 stream 사용

--- src/main/java/hanghae/restock/service/UserNotificationService.java
+++ src/main/java/hanghae/restock/service/UserNotificationService.java
@@ -1,9 +1,13 @@
 package hanghae.restock.service;
 
+import static hanghae.restock.service.common.util.ErrorMessage.*;
+
 import hanghae.restock.domain.productusernotification.ProductUserNotification;
 import hanghae.restock.domain.productusernotificationhistory.ProductUserNotificationHistory;
-import hanghae.restock.service.port.LocalDateTimeHolder;
+import hanghae.restock.service.common.util.ErrorMessage;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -15,34 +19,34 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserNotificationService {
     private final Map<Long, List<ProductUserNotificationHistory>> history = new ConcurrentHashMap<>();
-    private final LocalDateTimeHolder localDateTimeHolder;
     private final UserService userService;
 
     /**
-     * 상품 재입고 알림을 신청한 유저들을 찾는다
-     * 재입고 유저정보를 저장한다
+     * 상품 재입고 알림을 신청한 유저들을 찾는다 재입고 유저정보를 저장한다
      */
     public void processNotification(Long productId, Long restockPhase) {
         List<ProductUserNotification> users = userService.getActiveProductNotifiers(productId);
         users.forEach(i -> history.computeIfAbsent(restockPhase, k ->
-                new ArrayList<>()).add(createHistory(productId, i.getUserId(), restockPhase)));
+                new ArrayList<>()).add(createHistory(productId, i.getUserId(), restockPhase, i.getUpdatedAt())));
     }
 
-    private ProductUserNotificationHistory createHistory(Long productId, Long userId, Long restockPhase) {
+    private ProductUserNotificationHistory createHistory(Long productId, Long userId, Long restockPhase,
+                                                         LocalDateTime mostRecentDate) {
         return ProductUserNotificationHistory.create(
-                productId, userId, restockPhase, localDateTimeHolder.getCurrentDate()
+                productId, userId, restockPhase, mostRecentDate
         );
     }
 
     /**
      * 마지막으로 저장한 유저 Id를 반환한다
      */
-    public Long getLastNotificationUserId(Long productId, Long restockPhase) {
-        return Objects.requireNonNull(history.get(restockPhase)
-                        .stream()
-                        .max((a, b) -> b.getNotificationDate().compareTo(a.getNotificationDate()))
-                        .orElse(null))
-                .getUserId();
+    public Long getLastNotificationUserId(Long restockPhase) {
+        return Objects.requireNonNull(Objects.requireNonNull(history.get(restockPhase)
+                .stream()
+                .max(Comparator.comparing(ProductUserNotificationHistory::getNotificationDate))
+                .map(ProductUserNotificationHistory::getUserId)
+                .orElseThrow(() -> new IllegalArgumentException(NOT_FOUND_USER_NOTI_HISTORY.getMessage())))
+        );
     }
 
 }
--- src/main/java/hanghae/restock/service/common/util/ErrorMessage.java
+++ src/main/java/hanghae/restock/service/common/util/ErrorMessage.java
@@ -7,7 +7,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public enum ErrorMessage {
 
-    NOT_FOUND_PRODUCT("해당하는 상품을 찾을 수 없습니다.");
+    NOT_FOUND_PRODUCT("해당하는 상품을 찾을 수 없습니다."),
+    NOT_FOUND_USER_NOTI_HISTORY("해당 재입고 회차의 유저 정보가 없습니다."),
+    ;
 
     private final String message;
 }
